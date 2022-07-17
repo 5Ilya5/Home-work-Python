bdb52123607941a741a9e4ddd01b17c17a3e0160
5Ilya5 <102946779+5Ilya5@users.noreply.github.com>
2022-07-17 15:36:27 +0300

    Add files via upload
    домашняя работа по третьему семинару

---
+++ дз семинар 3 (1).py
@@ -0,0 +1,2 @@
+﻿my_list = [5, 6, 2, 3, 1]
+print(sum(my_list[1::2]))
---
+++ дз семинар 3 (3).py
@@ -0,0 +1,9 @@
+﻿my_list = [1.5, 1.9, 5.3, 5, 10.08]
+min = 1
+max = 0
+for i in my_list:
+    if (i - int(i)) <= min:
+        min = i - int(i)
+    if (i - int(i)) >= max:
+        max = i - int(i)  
+print(max-min)
